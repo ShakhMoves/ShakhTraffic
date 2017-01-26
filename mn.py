d9d85e33b61acdee3d882850e9c528bf39cb3554
Parham Alvani <parham.alvani@gmail.com>
2017-01-26 21:45:44 +0330

    Mininet main script was improved.

--- mn.py
+++ mn.py
@@ -10,11 +10,12 @@
 
 """
 mn.py:
-    Script for running our sample topology on mininet
+    Script for running fat tree topology on mininet
     and connect it into contorller on remote host.
+    it can have many remote controllers on remote hosts.
 Usage (example uses IP = 192.168.1.2):
     From the command line:
-        sudo python mn.py --ip 192.168.1.2
+        sudo python mn.py 192.168.1.2
 """
 import argparse
 from functools import partial
@@ -24,65 +25,30 @@ from mininet.net import CLI
 from mininet.log import setLogLevel
 from mininet.node import RemoteController
 from mininet.node import OVSSwitch
-from mininet.topo import Topo
 from mininet.topolib import TreeTopo
-
-class SampleTopology(Topo):
-    """
-    Subclass of mininet Topo class for
-    creating following topology:
-    h1 --- s1 -+- s2 --- h4
-    h2 --- |   |   | --- h5
-               s5
-               |
-    h3 --- s3 -+- s4 --- h6
-    """
-    def build(self, *args, **params):
-        switch1 = self.addSwitch(name='s1')
-        switch2 = self.addSwitch(name='s2')
-        switch3 = self.addSwitch(name='s3')
-        switch4 = self.addSwitch(name='s4')
-        switch5 = self.addSwitch(name='s5')
-        host1 = self.addHost(name='h1')
-        host2 = self.addHost(name='h2')
-        host3 = self.addHost(name='h3')
-        host4 = self.addHost(name='h4')
-        host5 = self.addHost(name='h5')
-        host6 = self.addHost(name='h6')
-        self.addLink(host1, switch1)
-        self.addLink(host2, switch1)
-        self.addLink(host4, switch2)
-        self.addLink(host5, switch2)
-        self.addLink(host3, switch3)
-        self.addLink(host6, switch4)
-        self.addLink(switch1, switch5)
-        self.addLink(switch2, switch5)
-        self.addLink(switch3, switch5)
-        self.addLink(switch4, switch5)
 
 
 if __name__ == '__main__':
-    # PARSER = argparse.ArgumentParser()
-    # PARSER.add_argument('ips', metavar='ip',
-    #                    help='ONOS Network Controllers IP Addresses',
-    #                    default=['127.0.0.1'], type=str, nargs='*')
-    # CLI_ARGS = PARSER.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ips', metavar='ip',
+                        help='ONOS Network Controllers IP Addresses',
+                        default=['127.0.0.1'], type=str, nargs='*')
+    cli_args = parser.parse_args()
 
     setLogLevel('info')
 
-    SWITCH = partial(OVSSwitch, protocols='OpenFlow10')
-    ports = [6633]
+    switch = partial(OVSSwitch, protocols='OpenFlow10')
 
-    RCS = []
-    for port in ports:
-        RCS.append(RemoteController('ONOS-%s' % port, ip='192.168.128.179', port=port))
-    NET = Mininet(topo=TreeTopo(4, 2), switch=SWITCH, build=False)
-    for rc in RCS:
-        NET.addController(rc)
-    NET.build()
-    NET.start()
-    h1, h2 = NET.get('h1', 'h2')
-    h1.sendCmd('ping -i 0.01 -c 100 10.0.0.3')
-    h2.sendCmd('ping -i 0.01 -c 100 10.0.0.4')
-    CLI(NET)
-    NET.stop()
+    rcs = []
+    for ip in cli_args.ips:
+        rcs.append(RemoteController('ONOS-%s' % ip, ip=ip, port=6633))
+    net = Mininet(topo=TreeTopo(4, 2), switch=switch, build=False)
+    for rc in rcs:
+        net.addController(rc)
+    net.build()
+    net.start()
+    # h1, h2 = net.get('h1', 'h2')
+    # h1.sendCmd('ping -i 0.01 -c 100 10.0.0.3')
+    # h2.sendCmd('ping -i 0.01 -c 100 10.0.0.4')
+    CLI(net)
+    net.stop()
